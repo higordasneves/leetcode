21ea1527ecfa692547d45226d83f97fcf5b1dfca
Higor Neves <higordasneves@gmail.com>
2023-03-01 18:00:13 -0300

    ex 118: pascal triangle

--- pascal-triangle/pascal-triangle.go
+++ pascal-triangle/pascal-triangle.go
@@ -1,5 +1,21 @@
 package pascal_triangle
 
 func generate(numRows int) [][]int {
-	return nil
+	if numRows < 1 {
+		return nil
+	}
+
+	result := [][]int{{1}}
+	for i := 0; i < numRows-1; i++ {
+		lastLine := result[len(result)-1]
+		newLine := []int{1}
+
+		for j := 0; j < len(lastLine)-1; j++ {
+			newLine = append(newLine, lastLine[j]+lastLine[j+1])
+		}
+		newLine = append(newLine, 1)
+		result = append(result, newLine)
+	}
+
+	return result
 }
