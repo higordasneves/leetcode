3361d82ef59a3e1884a5458f98c2fd95e8c40c0c
Higor Neves <higordasneves@gmail.com>
2023-03-02 13:02:46 -0300

    ex 1671: Minimum Number of Removals to Make Mountain Array

--- mountain/mountain.go
+++ mountain/mountain.go
@@ -1,5 +1,38 @@
 package mountain
 
 func minimumMountainRemovals(nums []int) int {
-	return 0
+	n := len(nums)
+	inc, dec := make([]int, n), make([]int, n)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				inc[i] = max(inc[i], inc[j]+1)
+			}
+		}
+	}
+
+	for i := n - 2; i > -1; i-- {
+		for j := n - 1; j > i; j-- {
+			if nums[i] > nums[j] {
+				dec[i] = max(dec[i], dec[j]+1)
+			}
+		}
+	}
+
+	res := 0
+	for i := 0; i < n; i++ {
+		if inc[i] > 0 && dec[i] > 0 {
+			res = max(res, inc[i]+dec[i])
+		}
+	}
+
+	return n - res - 1
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
--- mountain/mountain_test.go
+++ mountain/mountain_test.go
@@ -18,6 +18,21 @@ func Test_minimumMountainRemovals(t *testing.T) {
 			nums: []int{2, 1, 1, 5, 6, 2, 3, 1},
 			want: 3,
 		},
+		{
+			name: "example 3",
+			nums: []int{1, 2, 3, 4, 4, 3, 2, 1},
+			want: 1,
+		},
+		{
+			name: "example 4",
+			nums: []int{4, 3, 2, 1, 1, 2, 3, 1},
+			want: 4,
+		},
+		{
+			name: "example 5",
+			nums: []int{1, 16, 84, 9, 29, 71, 86, 79, 72, 12},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
